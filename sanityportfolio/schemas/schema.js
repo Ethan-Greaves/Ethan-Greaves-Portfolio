a83963681cf535af33edd7c05690870942c1988f
Ethan-Greaves <ethangreaves@hotmail.co.uk>
2021-02-07 23:48:44 +0000

    Added Colours to settings schema
    Created the settings schema for sanity.io.
    
    Added default CSS colours to the "theme colour" field in schema, as well as custom colour

--- sanityportfolio/schemas/schema.js
+++ sanityportfolio/schemas/schema.js
@@ -10,6 +10,7 @@ import category from './category';
 import post from './post';
 import author from './author';
 import project from './project';
+import settings from './settings/settings';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -22,8 +23,9 @@ export default createSchema({
 		// in the studio.
 		post,
 		author,
-    category,
-    project,
+		category,
+		project,
+		settings,
 		// When added to this list, object types can be used as
 		// { type: 'typename' } in other document schemas
 		blockContent,
---
+++ sanityportfolio/schemas/settings/settings.js
@@ -0,0 +1,54 @@
+import { defaultCSSColours, customColours } from './colours';
+import { colorToHex } from './colourConverters';
+
+const settings = {
+	name: 'settings',
+	title: 'Settings',
+	type: 'document',
+
+	fields: [
+		{
+			name: 'title',
+			title: 'Title',
+			type: 'string',
+		},
+		{
+			name: 'slug',
+			title: 'Slug',
+			type: 'slug',
+			options: {
+				source: 'title',
+				maxLength: 96,
+			},
+		},
+		{
+			title: 'Theme colour',
+			description:
+				'Will change the main colour of the portfolio website e.g. the colour of "Ethan Greaves." text on the landing page',
+			name: 'themeColour',
+			type: 'colorlist',
+			options: {
+				borderradius: {
+					outer: '100%',
+					inner: '100%',
+				},
+
+				title: 'hello',
+
+				list: defaultCSSColours
+					.map((colour) => {
+						return colorToHex(colour);
+					})
+					.map((coloursHexValue, i) => {
+						return {
+							title: defaultCSSColours[i],
+							value: coloursHexValue,
+						};
+					})
+					.concat(customColours),
+			},
+		},
+	],
+};
+
+export default settings;

0e3b665924264972277c5ab3b61245c2a8d088d5
Ethan-Greaves <ethangreaves@hotmail.co.uk>
2021-02-26 20:32:09 +0000

    Fixed about me section alignment

--- src/components/sections/aboutPage/aboutPage.js
+++ src/components/sections/aboutPage/aboutPage.js
@@ -11,27 +11,29 @@ const AboutPage = ({ ...authorData }) => {
 	const styles = aboutPageStyles();
 
 	return (
-		<Section title='about me' className={styles.text}>
-			<Grid container direction='row' justify='space-between' spacing={4} className={styles.grid}>
-				<Grid item xs={12} sm={9}>
-					<Typography color='secondary'>{authorData.bio} </Typography>
+		<Section title='about me'>
+			<div className={styles.text}>
+				<Grid container direction='row' justify='space-between' spacing={4} className={styles.grid}>
+					<Grid item xs={12} sm={9}>
+						<Typography color='secondary'>{authorData.bio} </Typography>
+					</Grid>
+
+					<Grid item xs={12} sm={3} align='center'>
+						<Avatar
+							variant='circular'
+							src={`${authorData.image}?h=300&w=300&fm=webp`}
+							className={styles.profileImg}
+							alt={`${authorData.name} profile image`}
+						/>
+					</Grid>
 				</Grid>
 
-				<Grid item xs={12} sm={3} align='center'>
-					<Avatar
-						variant='circular'
-						src={`${authorData.image}?h=300&w=300&fm=webp`}
-						className={styles.profileImg}
-						alt={`${authorData.name} profile image`}
-					/>
-				</Grid>
-			</Grid>
+				<Box mt={3} />
 
-			<Box mt={3} />
-
-			<ExternalLink to={authorData.cv} newTab={true} noopener={true}>
-				<Button className={buttonStyles().primaryBtn}>Download CV</Button>
-			</ExternalLink>
+				<ExternalLink to={authorData.cv} newTab={true} noopener={true}>
+					<Button className={buttonStyles().primaryBtn}>Download CV</Button>
+				</ExternalLink>
+			</div>
 		</Section>
 	);
 };
